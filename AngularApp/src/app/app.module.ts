3af00f171fe1c820040e2a440b371a772db57ac3
Srinath.Rayabarapu <Srinath.Rayabarapu@c58t5z1.causeway.local>
2018-09-09 20:25:32 +0530

    adding new components to angular app

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
